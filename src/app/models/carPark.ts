839bd1c25bc1662b9a6d848749fc17ccfd2d68c0
Vishnu Rana <mvishnurana@gmail.com>
2021-07-10 21:15:12 +0800

    Implemented interface for carPark type enum and driver

---
+++ src/app/models/carPark.ts
@@ -0,0 +1,5 @@
+export enum carPark {
+  '2P',
+  '3P',
+  '4P'
+}
---
+++ src/app/models/driver.ts
@@ -0,0 +1,8 @@
+import { carPark } from "./carPark";
+
+export class Driver {
+  firstName: String;
+  lastName?: String;
+  carParkType: carPark;
+  location?: String;
+}
