425fa9d1b78e45826fc985ec5804088b177b54f9
Vishnu Rana <mvishnurana@gmail.com>
2021-07-18 21:15:39 +0800

    Implemented a fake-data module for mocking valid application data

---
+++ src/app/fake-data/fake-data.ts
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    firstName: 'John Doe',
+    hour: 21,
+    minute: 15,
+    carParkType: '3P',
+    location: 'Lord\'s Gym'
+  },
+  {
+    firstName: 'Mike Dixon',
+    hour: 20,
+    minute: 30,
+    carParkType: '3P',
+    location: 'Lord\'s Gym'
+  },
+  {
+    firstName: 'Harvey Milk',
+    hour: 19,
+    minute: 15,
+    carParkType: '3P',
+    location: 'Lord\'s Gym'
+  },
+  {
+    firstName: 'SomeOne with a really long name',
+    hour: 21,
+    minute: 45,
+    carParkType: '3P',
+    location: 'Lord\'s Gym'
+  }
+]
