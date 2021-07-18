ae0de2ee713951650432220c19e04f85f1258f04
Vishnu Rana <mvishnurana@gmail.com>
2021-07-18 18:28:01 +0800

    Started using card-timer controller

--- src/app/card-timer/card-timer.component.ts
+++ src/app/card-timer/card-timer.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { carPark } from './../models/car-park';
+import { interval } from 'rxjs';
+import { Driver } from '../models/driver';
 
 @Component({
   selector: 'card-timer',
@@ -7,30 +8,61 @@ import { carPark } from './../models/car-park';
   styleUrls: ['./card-timer.component.scss']
 })
 export class CardTimerComponent implements OnInit {
+
   @Input()
+  driver: Driver;
+
+  timer: number;
+  remainingTime: number;
   time: number;
 
-  @Input()
-  carParkType: carPark;
-
-  getTimeDiff(): Date {
+  getTimeDifference(): number {
       const latestDate = new Date();
       const date = latestDate.getDate();
       const month = latestDate.getMonth();
       const year = latestDate.getFullYear();
 
-      console.log((new Date().valueOf() - new Date(this.time).valueOf()));
-      console.log('this time...', new Date(year, month, date, this.time));
-      console.log('latest...', new Date());
-      console.log('difference: ', Math.floor((new Date().valueOf() - new Date(this.time).valueOf()) / 6000));
-      return new Date(new Date().valueOf() - new Date(this.time).valueOf());
+      return Math.floor(((Date.now().valueOf() - new Date(year, month, date, this.driver.hour, this.driver.minute).valueOf()) / 1000) / 60);
+  }
+
+  totalTime(): number {
+
+    switch(this.driver.carParkType) {
+      case '1P':
+        return 60;
+
+      case '2P':
+        return 120;
+
+      case '3P':
+        return 180;
+
+      case '4P':
+        return 240;
+
+      default:
+        return -1;
+    }
+  }
+
+  newTimer(): void {
+    const difference = this.getTimeDifference();
+    const total = this.totalTime();
+
+    if (total !== -1) {
+      this.remainingTime = total - difference;
+
+      const source = interval(1000);
+
+      const subscribe = source.subscribe({
+        next: (val) => {
+          this.time = val;
+        }
+      })
+    }
   }
 
   ngOnInit() {
-    this.getTimeDiff();
-    // Get current time
-    // get the time parked
-    // Get the difference
-    // Initiate a counter on card
+    this.newTimer();
   }
 }
