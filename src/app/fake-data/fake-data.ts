5a0be6f87eeb994b0e6234afd2e5ffe4a527d22a
Vishnu Rana <mvishnurana@gmail.com>
2021-09-28 22:56:34 +0800

    Updated the flex layout when there\'s no data in the application to populate on the UI

--- src/app/fake-data/fake-data.ts
+++ src/app/fake-data/fake-data.ts
@@ -1,25 +1,25 @@
 export const data = [
   {
     firstName: 'John Doe',
-    timeParked: new Date(2021, 8, 27, 19, 24),
+    timeParked: new Date(2021, 8, 29, 12, 24),
     carParkType: '2P',
     location: 'Lord\'s Gym'
   },
   {
     firstName: 'Mike Dixon',
-    timeParked: new Date(2021, 8, 27, 18, 30),
+    timeParked: new Date(2021, 8, 29, 11, 30),
     carParkType: '3P',
     location: 'Lord\'s Gym'
   },
   {
     firstName: 'Harvey Milk',
-    timeParked: new Date(2021, 8, 27, 17, 30),
+    timeParked: new Date(2021, 8, 29, 10, 30),
     carParkType: '3P',
     location: 'Lord\'s Gym'
   },
   {
     firstName: 'SomeOne with a really long name',
-    timeParked: new Date(2021, 8, 27, 20, 0),
+    timeParked: new Date(2021, 8, 29, 9, 45),
     carParkType: '3P',
     location: 'Lord\'s Gym'
   }
