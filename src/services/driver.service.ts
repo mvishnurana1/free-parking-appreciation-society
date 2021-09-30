7acd4bbd0c92a099408b8625a74b150c5ecf2ee0
Vishnu Rana <mvishnurana@gmail.com>
2021-09-30 10:10:11 +0800

    Revert "Revert "WIP: Working on piping form-data to the components on UI" (#31)"
    This reverts commit 9006b6afd8f92fa099dcfd6203e2eedabd366f78.

--- src/services/driver.service.ts
+++ src/services/driver.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { data } from 'src/app/fake-data/fake-data';
 import { Driver } from 'src/app/models/driver';
 import { DateService } from './date.service';
 import { addHours } from 'date-fns';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DriverService {
-  drivers: Array<Driver> = data;
+  drivers: BehaviorSubject<Array<Driver>> = new BehaviorSubject<Array<Driver>>([]);
 
   constructor(
     private readonly dateService: DateService,
@@ -19,5 +19,9 @@ export class DriverService {
     const parkedTill = addHours(driver.timeParked, carParkType);
 
     return this.dateService.timeDifferenceInMinutes(parkedTill, new Date(Date.now()));
+  }
+
+  addDriver(driver: Driver):  void {
+    this.drivers.next(this.drivers.getValue().concat([driver]));
   }
 }
