cd7fa91cd2e2408a1923ac893661cb1a1f20d1f4
Vishnu Rana <vishnu@studentedge.com.au>
2021-10-19 16:21:50 +0800

    fixed tests in the project

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "preset": "jest-preset-angular",
   "setupFilesAfterEnv": [
-      "<rootDir>/setup-jest.ts"
+      "./setup-jest.ts"
   ],
   "transformIgnorePatterns": [
       "node_modules/(?!@ngrx|ngx-socket-io)" // List any packages here that error
@@ -10,9 +10,8 @@ module.exports = {
       "^.+\\.(ts|js|html)$": "ts-jest"
   },
   "testPathIgnorePatterns": [
-      "<rootDir>/node_modules/",
+    "<rootDir>/node_modules/",
       "<rootDir>/dist/",
       "<rootDir>/cypress/",
-      "<rootDir>/src/test.ts",
   ]
 };
--- src/app/driver-cards/driver-cards.spec.ts
+++ src/app/driver-cards/driver-cards.spec.ts
@@ -1,5 +1,20 @@
 import { DriverCardsComponent } from './driver-cards.component';
 
 describe('DriverCardsComponent', () => {
-  
+  let fixture: DriverCardsComponent;
+  let driverServiceMock: any;
+
+  beforeEach(() => {
+    driverServiceMock = {
+
+    };
+
+    fixture = new DriverCardsComponent(
+      driverServiceMock
+    );
+
+    describe('Setup component: ', () => {
+      fixture.drivers$ = driverServiceMock;
+    })
+  })
 })
